df0f84a9bf13fb70372db96ce08aef1e646866ac
ray <ray.g@gmx.de>
2015-07-15 02:20:26 +0200

    possible fix

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@ green='\033[0;32m'
 red='\033[0;31m'
 NC='\033[0m'
 
-PATH=`pwd -P`
-echo $PATH
+#CP == current path
+CP=`pwd -P`
+echo $CP
 
 echo "check if .vimrc is a link"
 if [[ -L "$HOME/.vimrc" ]];then
   echo -e "${green}already a link. Nothing to do here.${NC}"
 else
   echo -e "${red}not a link. Deleting file and creating link.${NC}"
-  echo "`rm $HOME/.vimrc`"
-  echo "`ln -s $PATH/.vimrc $HOME/.vimrc`"
+  rm $HOME/.vimrc
+  ln -s $CP/.vimrc $HOME/.vimrc
 fi
 
 echo "check if .bash_profile is a link"
@@ -21,8 +22,8 @@ if [[ -L "$HOME/.bash_profile" ]];then
   echo -e "${green}already a link. Nothing to do here.${NC}"
 else
   echo -e "${red}not a link. Deleting file and creating link.${NC}"
-  echo "`rm $HOME/.bash_profile`"
-  echo "`ln -s $PATH/.bash_profile $HOME/.bash_profile`"
+  rm $HOME/.bash_profile
+  ln -s $CP/.bash_profile $HOME/.bash_profile
 fi
 
 echo "check if .inputrc is a link"
@@ -30,11 +31,11 @@ if [[ -L "$HOME/.inputrc" ]];then
   echo -e "${green}already a link. Nothing to do here.${NC}"
 else
   echo -e "${red}not a link. Deleting file and creating link.${NC}"
-  echo "`rm $HOME/.inputrc`"
-  echo "`ln -s $PATH/.inputrc $HOME/.inputrc`"
+  rm $HOME/.inputrc
+  ln -s $CP/.inputrc $HOME/.inputrc
 fi
 
 echo -e "${green}done with all the link${NC}"
 echo "check the vim bundles now"
 
-#/bin/sh $PATH/bundles.sh
+#/bin/sh $CP/bundles.sh
